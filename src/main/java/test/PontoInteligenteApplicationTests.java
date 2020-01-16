4692706318335721da1061afbde75704da73d99c
izaura <izaura.fsilva@gmail.com>
2020-01-16 15:34:26 -0300

    add package test

---
+++ src/main/java/test/PontoInteligenteApplicationTests.java
@@ -0,0 +1,21 @@
+package test;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class PontoInteligenteApplicationTests {
+
+	@Test
+	@Ignore
+	public void contextLoads() {
+	}
+
+}
