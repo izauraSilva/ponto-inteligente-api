a6e13a97234883786f69b327fcf79d16386f4d10
izaura <izaura.fsilva@gmail.com>
2020-02-02 01:05:46 -0300

    add class test

--- src/main/java/com/izatec/pontointeligente/api/repository/EmpresaRepository.java
+++ src/main/java/com/izatec/pontointeligente/api/repository/EmpresaRepository.java
@@ -1,14 +1,14 @@
 package com.izatec.pontointeligente.api.repository;
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.izatec.pontointeligente.api.entities.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 
-    @Transactional(readOnly = true)
+    //@Transactional(readOnly = true)
     Empresa findByCnpj(String cnpj);
 
 }
